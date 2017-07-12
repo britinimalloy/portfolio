88a6800021d253a21f6aef6879cd560da72e821d
britinimalloy <britini.malloy@outlook.com>
2017-07-12 13:51:55 -0700

    added missing files back in, populated with the content they were supposed to have. added to modules and base.css files, index.html

---
+++ js/projectArticles.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var projectData = [
+  {
+    title:       'Busmall',
+    image:       '',
+    description: ''
+  },
+  {
+    title:       'Smart Quiz',
+    image:       '',
+    description: ''
+  },
+  {
+    title:       'Cookie Stand',
+    image:       '',
+    description: ''
+  },
+];
