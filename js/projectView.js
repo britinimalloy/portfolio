8ae8f69ab9ed90eb0393887e1334d34301cc3f97
britinimalloy <britini.malloy@outlook.com>
2017-07-14 11:56:34 -0700

    added projectView.js and some content to it to start handling clicking events

---
+++ js/projectView.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var projectView = {};
+
+projectView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function () {
+    $('.tab-content').hide();
+  })
+};
+
+$(document).ready(function () {
+  projectView.handleMainNav();
+})
