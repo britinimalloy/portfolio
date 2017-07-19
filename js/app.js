ddf06615e0e0949708106df858989a7176be4367
britinimalloy <britini.malloy@outlook.com>
2017-07-19 12:28:00 -0700

    added toHtml() and appended (hopefully that works) to the corresponding HTML div

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var projects = [];
+
 // ===========================
 // ========CONSTRUCTOR========
 // ===========================
@@ -8,3 +10,17 @@ function Projects (projectDataObj) {
   this.img = projectDataObj.img;
   this.description = projectDataObj.description;
 }
+
+Projects.prototype.toHtml = function() {
+  var projectTemplateString = $('#projectsHandlebarTemplate').html();
+  var compiledProject = Handlebars.compile(projectTemplateString);
+  return compiledProject(projectTemplateString);
+}
+
+projectData.forEach(function(projectObject) {
+  projects.push(new Projects(projectObject));
+});
+
+projects.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+});
