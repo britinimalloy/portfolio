311f8e81c0ad79f75441d92f95c9d1b1ea713722
britinimalloy <britini.malloy@outlook.com>
2017-07-25 10:40:36 -0700

    all this fixing had me confused on which things belong in which assignment

--- js/app.js
+++ js/app.js
@@ -11,39 +11,9 @@ function Projects (projectDataObj) {
   this.description = projectDataObj.description;
 }
 
-//Projects.prototype.toHtml = function() {
-//  var projectTemplateString = $('#projectsHandlebarTemplate').html();
-//  var compiledProject = Handlebars.compile(projectTemplateString);
-//  return compiledProject(projectTemplateString);
-//}
-
-//projectData.forEach(function(projectObject) {
-//  projects.push(new Projects(projectObject));
-//});
-
-//projects.forEach(function(project) {
-//  $('#projects').append(project.toHtml());
-//});
-
-// function addNewElementForCloning () {
-//   var newListElement = $('.LItemplateUL').clone();
-//   newListElement.text('here is another li, this is silly');
-//   console.log(newListElement);
-//   $('ul').append(newListElement);
-// }
-// addNewElementForCloning();
-
 var projects = [];
 
 Projects.all = [];
-
-Projects.prototype.toHtml = function () {
-  var hTemplateInfo = $('#projectsHandlebarTemplate').html();
-
-  var compiledProject = Handlebars.compile(hTemplateInfo);
-
-  return compiledProject(this);
-}
 
 projectData.forEach(function(projectDataObj) {
   projects.push(new Projects(projectDataObj));
--- js/projectView.js
+++ js/projectView.js
@@ -3,17 +3,15 @@
 var projectView = {};
 
 projectView.handleMainNav = function () {
-  // $('.main-nav').on('click', '.tab', function () {
-  //   $('.tab-content').hide();
-  // })
+  $('.main-nav').on('click', '.tab', function () {
+    $('.tab-content').hide();
+  })
 };
 
 projectView.handleHamburgerBehavior = function () {
   $('#hamburger-icon').on('click', function () {
     $('ul.nav').children().show();
     $('#hamburger-icon').hide();
-    console.log('hello');
-    console.log(this);
   })
 }
 
