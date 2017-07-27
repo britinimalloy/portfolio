348aaa042d6548b8eabfedc0b60b571bad97f0ff
britinimalloy <britini.malloy@outlook.com>
2017-07-27 14:48:18 -0700

    closer to a working version for Heroku

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy');
 const app = express();
-let conString = process.env.CONNECTION_STRING || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/kilovolt`;
+var conString = process.env.CONNECTION_STRING || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/kilovolt`;
+//let conString = process.env.CONNECTION_STRING || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/kilovolt`;
+const PORT = process.env.PORT || 3000;app.listen(PORT, function() {
+  console.log(`App is listening on: ${PORT}`);
+})
 const client = new pg.Client(conString);
 const proxy = require('express-request-proxy');
