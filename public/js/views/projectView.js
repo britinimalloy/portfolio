bac16f87cfabe0cbe7bc0be22d48b4da7a99df66
britinimalloy <britini.malloy@outlook.com>
2017-07-30 13:47:26 -0700

    Yay, the hiding and showing now works! Except the projects section loses its content...

--- public/js/views/projectView.js
+++ public/js/views/projectView.js
@@ -3,49 +3,23 @@
 var projectView = {};
 
 projectView.handleMainNav = function () {
+  // click on the nav links to hide all sections, then show only the selected one
   // The following content is help from Aeone:
-  // =================click on nav-item hides other content
-  $('.nav-tabs').on('click', function(event){
-    // ===============event.preventDefault();
-    // ===============to prevent from taking the browser to a new URL
-    event.preventDefault();
+  $('.nav-tabs').on('click', function(){
     $('.tab-content').hide();
-  // =================reveals only nav-item clicked
     var selected = $(this).attr('id');
     console.log(selected);
-    // ===============dynamically build selector with correct ID based on data
-    if (selected === '#nav-about') {
+
+    if (selected === 'nav-about') {
       $('#about').show();
-    } else if (selected === '#nav-projects') {
+    } else if (selected === 'nav-projects') {
       $('#projects').show();
     } else {
       $('#main').show();
     }
-    // ================$('section[id="' + selected + '"]').show();
   });
   $('#main-nav .nav-tabs:first').click();
 };
-
-// $('.main-nav').on('click', '.tab', function () {
-//   from starter code lab 5:
-//   if ($(this) === $('#main')) {
-//     $('#list-stuff').hide();
-//     $('#about').hide();
-//     $('#projects').hide();
-//   } else if ($(this) === $('#about')) {
-//     $('#main').hide();
-//     $('#list-stuff').hide();
-//     $('#projects').hide();
-//   } else {
-//     $('#main').hide();
-//     $('#about').hide();
-//     $('#list-stuff').hide();
-//   }
-//   $('.tab-content').hide();
-//   from starter code lab 4:
-//   $('#' + $(this).data('content')).fadeIn();
-// });
-// $('.main-nav .tab:first').click();
 
 projectView.handleHamburgerBehavior = function () {
   $('#hamburger-icon').on('click', function () {
@@ -54,17 +28,7 @@ projectView.handleHamburgerBehavior = function () {
   })
 };
 
-// hide everything but main (and footer section) on page load
-projectView.pageInit = function () {
-  // $('.tab-content').hide();
-  $('#main').show();
-  $('#list-stuff').hide();
-  $('#about').hide();
-  $('#projects').hide();
-};
-
 $(document).ready(function () {
-  // projectView.pageInit();
   projectView.handleMainNav();
   projectView.handleHamburgerBehavior();
 })
