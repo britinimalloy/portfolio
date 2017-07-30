f81fe827ffbe2f2f681f9b94f44b5c373a5410bb
britinimalloy <britini.malloy@outlook.com>
2017-07-30 13:24:19 -0700

    getting there with the hide/show

--- public/js/views/projectView.js
+++ public/js/views/projectView.js
@@ -3,27 +3,49 @@
 var projectView = {};
 
 projectView.handleMainNav = function () {
-  $('.main-nav').on('click', '.tab', function () {
-    // from starter code lab 5:
-    // if ($(this) === $('#main')) {
-    //   $('#list-stuff').hide();
-    //   $('#about').hide();
-    //   $('#projects').hide();
-    // } else if ($(this) === $('#about')) {
-    //   $('#main').hide();
-    //   $('#list-stuff').hide();
-    //   $('#projects').hide();
-    // } else {
-    //   $('#main').hide();
-    //   $('#about').hide();
-    //   $('#list-stuff').hide();
-    // }
-    // $('.tab-content').hide();
-    // from starter code lab 4:
-    // $('#' + $(this).data('content')).fadeIn();
+  // The following content is help from Aeone:
+  // =================click on nav-item hides other content
+  $('.nav-tabs').on('click', function(event){
+    // ===============event.preventDefault();
+    // ===============to prevent from taking the browser to a new URL
+    event.preventDefault();
+    $('.tab-content').hide();
+  // =================reveals only nav-item clicked
+    var selected = $(this).attr('id');
+    console.log(selected);
+    // ===============dynamically build selector with correct ID based on data
+    if (selected === '#nav-about') {
+      $('#about').show();
+    } else if (selected === '#nav-projects') {
+      $('#projects').show();
+    } else {
+      $('#main').show();
+    }
+    // ================$('section[id="' + selected + '"]').show();
   });
-  $('.main-nav .tab:first').click();
+  $('#main-nav .nav-tabs:first').click();
 };
+
+// $('.main-nav').on('click', '.tab', function () {
+//   from starter code lab 5:
+//   if ($(this) === $('#main')) {
+//     $('#list-stuff').hide();
+//     $('#about').hide();
+//     $('#projects').hide();
+//   } else if ($(this) === $('#about')) {
+//     $('#main').hide();
+//     $('#list-stuff').hide();
+//     $('#projects').hide();
+//   } else {
+//     $('#main').hide();
+//     $('#about').hide();
+//     $('#list-stuff').hide();
+//   }
+//   $('.tab-content').hide();
+//   from starter code lab 4:
+//   $('#' + $(this).data('content')).fadeIn();
+// });
+// $('.main-nav .tab:first').click();
 
 projectView.handleHamburgerBehavior = function () {
   $('#hamburger-icon').on('click', function () {
