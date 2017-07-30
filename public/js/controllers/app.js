97d86ef24c04dc5e293016dd631239a1273ab7af
britinimalloy <britini.malloy@outlook.com>
2017-07-30 16:49:55 -0700

    more fixes to incorporate all the code that was in the previous branch

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -24,3 +24,36 @@ projectData.forEach(function(projectDataObj) {
 projects.forEach(function(project){
   $('#projects').append(project.toHtml());
 });
+
+function addNewElementForCloning () {
+  var newListElement = $('.LItemplateUL').clone();
+  newListElement.text('here is another li, this is silly');
+  $('ul.nonsense').append(newListElement);
+}
+addNewElementForCloning();
+
+
+// Projects.prototype.toHtml = function () {
+//   var $template = $('#projects').clone();
+//
+//   $template.find('header > h1').text(this.name);
+//   $template.find('div > img').text(this.img);
+//   $template.find('section').text(this.description);
+//   return $template;
+// }
+
+// Projects.prototype.toHtml = function() {
+//   var projectTemplateString = $('#projectsHandlebarTemplate').html();
+//   console.log(projectTemplateString);
+//   var compiledProject = Handlebars.compile(projectTemplateString);
+//   console.log(compiledProject);
+//   return compiledProject(projectTemplateString);
+// }
+
+// Projects.prototype.toHtml = function() {
+//   var projectTemplateString = Handlebars.compile($('#projectsHandlebarTemplate').text());
+//   console.log(projectTemplateString);
+//   var compiledProject = Handlebars.compile(projectTemplateString);
+//   console.log(compiledProject);
+//   return projectTemplateString(this);
+// }
