38b82541bdb5e7c8e8c27b02f34809517852e99f
britinimalloy <britini.malloy@outlook.com>
2017-07-30 22:22:31 -0700

    added fetchAll() to app.js, script tag to index.html

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -27,36 +27,23 @@ projects.forEach(function(project){
   $('#projects').append(project.toHtml());
 });
 
+Projects.fetchAll = function() {
+  if (localStorage.projectData) {
+    Projects.loadAll(JSON.parse(localStorage.projectData))
+  } else {
+    $.getJSON('data/projectArticles.json')
+    .then(function(projectData) {
+      Projects.loadAll(projectData);
+      localStorage.projectData = JSON.stringify(projectData);
+    }, function(err) {
+      console.error(err);
+    });
+  }
+}
+
 function addNewElementForCloning () {
   var newListElement = $('.LItemplateUL').clone();
   newListElement.text('here is another li, this is silly');
   $('ul.nonsense').append(newListElement);
 }
 addNewElementForCloning();
-
-
-
-// Projects.prototype.toHtml = function () {
-//   var $template = $('#projects').clone();
-//
-//   $template.find('header > h1').text(this.name);
-//   $template.find('div > img').text(this.img);
-//   $template.find('section').text(this.description);
-//   return $template;
-// }
-
-// Projects.prototype.toHtml = function() {
-//   var projectTemplateString = $('#projectsHandlebarTemplate').html();
-//   console.log(projectTemplateString);
-//   var compiledProject = Handlebars.compile(projectTemplateString);
-//   console.log(compiledProject);
-//   return compiledProject(projectTemplateString);
-// }
-
-// Projects.prototype.toHtml = function() {
-//   var projectTemplateString = Handlebars.compile($('#projectsHandlebarTemplate').text());
-//   console.log(projectTemplateString);
-//   var compiledProject = Handlebars.compile(projectTemplateString);
-//   console.log(compiledProject);
-//   return projectTemplateString(this);
-// }
